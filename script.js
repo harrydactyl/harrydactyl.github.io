a8fa29cb7eeb105cca33005d4057b039118c3863
harrydactyl <Harrydactyl@Harrys-MacBook-Air.local>
2014-11-02 13:43:09 -0500

    Version 4.3
    Tweaks

--- script.js
+++ script.js
@@ -1,98 +1,89 @@
-var quiz={
+var quiz = {
     "questions": [{
         "question": "What year was Trinity founded?",
-            "1": "1910",
-            "2": "1709",
-            "3": "1802",
-            "4": "1950",
-            "5": "1800",
-            "correct": "1709",
+        "1": "1910",
+        "2": "1709",
+        "3": "1802",
+        "4": "1950",
+        "5": "1800",
+        "correct": "1709",
         "image": "\"old person\""
     }, {
         "question": "What is Trinity's student:teacher ratio?",
-            "1": "6:1",
-            "2": "5:1",
-            "3": "10:1",
-            "correct": "6:1",
+        "1": "6:1",
+        "2": "5:1",
+        "3": "10:1",
+        "correct": "6:1",
         "image": "\"student teacher\""
     }, {
         "question": "What are Trinity's school colors?",
-            "1": "Blue and Gold",
-            "2": "White and Blue",
-            "3": "Green and Gold",
-            "4": "Purple and White",
-            "correct": "Blue and Gold",
+        "1": "Blue and Gold",
+        "2": "White and Blue",
+        "3": "Green and Gold",
+        "4": "Purple and White",
+        "correct": "Blue and Gold",
         "image": "\"blue and gold\""
     }, {
         "question": "How big is the Trinity senate?",
-            "1": "8 students",
-            "2": "4 students",
-            "3": "16 students",
-            "correct": "16 students",
+        "1": "8 students",
+        "2": "4 students",
+        "3": "16 students",
+        "correct": "16 students",
         "image": "\"US senate\""
     }, {
         "question": "How many varsity teams does the upper school have in the winter season?",
-            "1": "8 teams",
-            "2": "6 teams",
-            "3": "4 teams",
-            "4": "5 teams",
-            "5": "7 teams",
-            "correct": "6 teams",
+        "1": "8 teams",
+        "2": "6 teams",
+        "3": "4 teams",
+        "4": "5 teams",
+        "5": "7 teams",
+        "correct": "6 teams",
         "image": "\"wrestler\""
     }, {
         "question": "On what street is Trinity located?",
-            "1": "91st st",
-            "2": "100th",
-            "3": "84th",
-            "correct": "91st",
+        "1": "91st st",
+        "2": "100th",
+        "3": "84th",
+        "correct": "91st",
         "image": "\"street sign\""
     }, {
         "question": "On what floor is Trinity's math lab",
-            "1": "The first floor",
-            "2": "The third floor",
-            "3": "The second floor",
-            "4": "The basement",
-            "correct": "The second floor",
+        "1": "The first floor",
+        "2": "The third floor",
+        "3": "The second floor",
+        "4": "The basement",
+        "correct": "The second floor",
         "image": "\"calculus\""
     }, {
         "question": "How many semester(s) of art does the Upper School require?",
-            "1": "Three semesters",
-            "2": "One semester",
-            "3": "Two semesters",
-            "4": "No semesters of art are required",
-            "correct": "Three semesters",
+        "1": "Three semesters",
+        "2": "One semester",
+        "3": "Two semesters",
+        "4": "No semesters of art are required",
+        "correct": "Three semesters",
         "image": "\"student painting\""
     }, {
         "question": "How many pool(s) does Trinity have?",
-            "1": "One pool",
-            "2": "Two pools",
-            "3": "No pools",
-            "correct": "Two pools",
+        "1": "One pool",
+        "2": "Two pools",
+        "3": "No pools",
+        "correct": "Two pools",
         "image": "\"swimmer\""
     }, {
         "question": "Which Canadian province is part of Trinity's global travel program?",
-            "1": "British Columbia",
-            "2": "Ontario",
-            "3": "Quebec",
-            "correct": "Quebec",
+        "1": "British Columbia",
+        "2": "Ontario",
+        "3": "Quebec",
+        "correct": "Quebec",
         "image": "\"canadians\""
-    }]                                  
+    }]
+};
 
-}
-/*$(document).ready(function(){
-        var data;
-        data= $.getJSON("questions.json", function(data) {
-        quiz = data;
-    });
-});//end jQuery
-alert(quiz);
-*/
-
-var score= new Array(quiz.questions.length);
-var current=-1;
+var score = new Array(quiz.questions.length);
+var current = -1;
 
 function firstNext() {
-    if(localStorage.getItem("accountExists")=="true"){
+     if(localStorage.getItem("accountExists")=="true"){
         logIn();
     }
     else{
@@ -118,6 +109,12 @@ function firstNext() {
         show();
     }
 }
+
+/*function doNext(){
+    $("#entireQuiz").fadeOut("slow", next() {
+    // Animation complete.
+  });
+}*/
 
 function next() {
     //localStorage();
@@ -150,25 +147,23 @@ function back() {
     show();
 }
 
-function show() {
+function show(){
     if(current<quiz.questions.length){
         var html = "";
-        //alert(quiz.questions[current][1]);
-        //alert("working0");
+        console.log(quiz.questions[current].question);
         html += "<br> <b><u>" + quiz.questions[current].question + "</u></b><br><br>";
-        //alert("working0");
         var size=howLong();
         for (i = 1; i <=size; i++) {
             var num = i.toString();
             console.log(num + ": " + quiz.questions[current][i]);
             html += "<input type=\"radio\" name=\"choice\">";
+            html+=quiz.questions[current][i];
             html += "<br><br>";
         }
         getFlickr();
-        html+=quiz.questions[current].image;
-        html += "<br><br>";
+        html += "<br>";
         html += "<button type=\"button\" onclick=\"back()\">Back</button><button type=\"button\" onclick=\"next()\">Next</button><br><br>";
-
+        html+=quiz.questions[current].image;
         document.getElementById("entireQuiz").innerHTML = html;
         document.getElementById("greeting").innerHTML= "<br>"+"You're so smart! I know you'll get a perfect score, "+document.getElementById("username").value+"."+"<br><br>";
     }
