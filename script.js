17b5338cb5f4f74c949d274407ffcf474efc363f
harrydactyl <Harrydactyl@Harrys-MacBook-Air.local>
2014-11-20 22:43:21 -0500

    3

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ var form = 1;
 var current;
 
 function loadJSON() {
+    alert(1);
     hold = $.getJSON("form.json", function(data) {
+        alert(2);
         form=data;
+        alert(3);
     });
+    alert(4);
     if(form != 1) {
+        alert(5);
         //generateForm();
     }
+    alert(6);
 }
 /*
 function generateForm() {
