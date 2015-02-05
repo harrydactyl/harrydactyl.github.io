0c9f6d783444aac06554ebe11e0efa4d8b01b050
harrydactyl <Harrydactyl@Harrys-MacBook-Air.local>
2015-02-04 22:57:04 -0500

    Triangles

--- tests.js
+++ tests.js
@@ -1,47 +1,42 @@
-function startTests(){
-    //triangle exists
+function test(){
     QUnit.test("triangle exists (3,4,5)", function(assert){
         assert.equal(triangleExists(3,4,5), true);
     });
-    QUnit.test("triangle exists (3,3,5)", function(assert){
-        assert.equal(triangleExists(3,3,5), true);
+    QUnit.test("triangle exists (7,3,20)", function(assert){
+        assert.equal(triangleExists(3,3,5), false);
     });
-    QUnit.test("triangle exists (0,4,5)", function(assert){
-        assert.equal(triangleExists(0,4,5), false);
-    });
-    QUnit.test("triangle exists (3,3,3)", function(assert){
-        assert.equal(triangleExists(3,4,5), true);
+    QUnit.test("triangle exists (-1,-1,-1)", function(assert){
+        assert.equal(triangleExists(0,4,5), true);
     });
     
-    //triangle type
-    QUnit.test("triangle type (3,3,3)", function(assert){
-        assert.equal(triangleType(3,3,3), "acute");
-    });
     QUnit.test("triangle type (3,4,5)", function(assert){
         assert.equal(triangleType(3,4,5), "right");
     });
     QUnit.test("triangle type (3,3,10)", function(assert){
         assert.equal(triangleType(3,3,10), "obtuse");
     });
+    QUnit.test("triangle type (3,3,3)", function(assert){
+        assert.equal(triangleType(3,3,3), "acute");
+    });
 }
 
 function triangleExists(a,b,c){
-    var sides= [a,b,c];
-    sides.sort();
-    if(sides[0]+sides[1]>=sides[2] && sides[0]+sides[2]>=sides[1] && sides[1]+sides[2]>=sides[0]){
-        return true;
+    if(a+b>=c && a+c>=b && b+c>=a && a>=0 && b>=0 && c>=0){
+        return(true);
     }
-    return false;
+    else{
+        return(false);
+    }
 }
 
 function triangleType(a,b,c){
-    if(a*a+b*b==c*c){
-        return "right";
+    if(Math.pow(a,2)+Math.pow(b,2)==Math.pow(c,2)){
+        return("right");
     }
-    else if(a*a+b*b>c*c){
-        return "acute";
+    else if(Math.pow(a,2)+Math.pow(b,2)<Math.pow(c,2)){
+        return("obtuse");
     }
     else{
-        return "obtuse";
+        return("acute");
     }
 }
