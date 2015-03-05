dfcdc8aa6d41cbd79b4f04511bb9ad94ef9d06d7
harrydactyl <Harrydactyl@Harrys-MacBook-Air.local>
2015-03-04 20:01:25 -0500

    Test

--- script.js
+++ script.js
@@ -1,4 +1,64 @@
-var clickX = new Array();
+(function() {
+    var canvas = document.getElementById('paint');
+    var ctx = canvas.getContext('2d');
+	var imageObj = new Image();
+	
+	/* Loading the Image*/
+	imageObj.onload = function() {
+        ctx.drawImage(imageObj, 0, 0);
+		/* Setting the paint to be Underneath the Drawing */
+		ctx.globalCompositeOperation="destination-over";
+		};
+	  
+      imageObj.src = 'https://lh5.googleusercontent.com/-P5ucC3TjCLU/UjHE0rENTaI/AAAAAAAAAts/mH2A_OORkQY/s800/color.png';
+
+
+    var mouse = {x: 0, y: 0};
+    var last_mouse = {x: 0, y: 0};
+
+    /* Mouse Capturing Work */
+    canvas.addEventListener('mousemove', function(e) {
+        last_mouse.x = mouse.x;
+        last_mouse.y = mouse.y;
+
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    }, false);
+
+	var brushColor = 'yellow';
+	
+     document.getElementById('brushBlue').onclick = function () {
+     brushColor = 'blue';
+     ctx.strokeStyle = brushColor;
+    };
+
+    /* Drawing on Paint App */
+    ctx.lineWidth = 20;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+	ctx.strokeStyle = brushColor;
+
+	canvas.addEventListener('mousedown', function(e) {
+        canvas.addEventListener('mousemove', onPaint, false);
+    }, false);
+
+    canvas.addEventListener('mouseup', function() {
+        canvas.removeEventListener('mousemove', onPaint, false);
+    }, false);
+
+    var onPaint = function() {
+        ctx.beginPath();
+        ctx.moveTo(last_mouse.x, last_mouse.y);
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.closePath();
+        ctx.stroke();
+		
+
+    };
+
+}());
+
+/*var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
 var paint;
@@ -52,4 +112,4 @@ function redraw(){
      context.closePath();
      context.stroke();
   }
-}
+}*/
